a96b1a05ca89b9a8ad462ff190c2e11b03a80c62
fs <fritzsvensson@live.se>
2024-04-06 19:01:24 +0200

    Initial commit: Scraper

---
+++ fin_scraper/fin_scraper/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class FinScraperItem(scrapy.Item):
+    title = scrapy.Field()
+    author = scrapy.Field()
+    publication_date = scrapy.Field()
+    article_text = scrapy.Field()
+    publication = scrapy.Field()
