abd21a35c6c2da4f6f70d7f5c7192ceaecf4b753
fs <fritzsvensson@live.se>
2024-04-15 19:45:13 +0200

    Added additinal graphs in tab 3 and 4

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from streamlit_gsheets import GSheetsConnection
 
 
 def load_data(news):
-    #news = pd.read_csv(file_path, dtype={'collection_date': 'string'})
     news['collection_date'] = pd.to_datetime(news['collection_date'], format='%Y-%m-%d', errors='coerce')
     
     # Check for rows where 'collection_date' could not be parsed and drop them
@@ -50,8 +49,7 @@ def create_plot(data, x, y, title, orientation='h', plot_type='bar', color='avg_
         fig = px.bar(data, x=x, y=y, orientation=orientation, color=color,
                      color_continuous_scale=px.colors.sequential.__dict__[color_scale], title=title)
     elif plot_type == 'line':
-        fig = px.line(data, x=x, y=y, color=color, title=title,
-                      color_continuous_scale=px.colors.sequential.__dict__[color_scale])
+        fig = px.line(data, x=x, y=y, color=color, title=title)
     # Add more plot types when needed
     return fig
 
@@ -68,6 +66,19 @@ def setup_bar_chart_df(df):
     top_tokens = token_data.nlargest(25, 'count').sort_values(by='count', ascending=False)
     return top_tokens
 
+def setup_sentiment_over_time(df):
+    top_tokens = df['tokens'].value_counts().nlargest(100).index
+    # Filter the dataset to include only the top tokens
+    df_top = df[df['tokens'].isin(top_tokens)]
+    df_avg = df_top.groupby(['collection_date', 'tokens']).sentiment.mean().reset_index()
+    return df_avg
+  
+def setup_occurrence_over_time(df):
+    top_tokens = df['tokens'].value_counts().nlargest(100).index
+    # Filter the dataset to include only the top tokens
+    df_top = df[df['tokens'].isin(top_tokens)]
+    df_occurrences = df_top.groupby(['collection_date', 'tokens']).size().reset_index(name='counts')
+    return df_occurrences
 
 ## main 
 conn = st.connection("gsheets", type=GSheetsConnection)
@@ -84,6 +95,8 @@ news_by_date = filter_data_by_date(news_long, reference_date)
 
 top_tokens_by_date = setup_bar_chart_df(news_by_date)
 top_tokens_all_time = setup_bar_chart_df(news_long)
+avg_token_sentiment_over_time = setup_sentiment_over_time(news_long)
+token_occurrence_over_time = setup_occurrence_over_time(news_long)
 
 # Create plots
 all_time_chart = create_plot(top_tokens_all_time, x='count', y='tokens', title="Token Sentiment Analysis (All Time)",
@@ -92,8 +105,9 @@ all_time_chart = create_plot(top_tokens_all_time, x='count', y='tokens', title="
 selected_day_chart = create_plot(top_tokens_by_date, x='count', y='tokens', title=f"Token Sentiment Analysis ({reference_date})",
                                  color_scale='Viridis')
 
+
 # Using tabs
-tab1, tab2, tab3 = st.tabs(["All time data", "Selected Date Data", "Testing"])
+tab1, tab2, tab3, tab4 = st.tabs(["All time data", "Selected Date Data", "Word sentiment over time", "Word occurrence over time"])
 
 st.markdown("# Python and data analysis practice page 🎈 \n Currently scraping financial news off zerohedge and CNBC and experimenting with the results \n \n Link to the repo: https://github.com/covfefefritz/scraper-project")
 
@@ -106,4 +120,21 @@ with tab2:
     st.plotly_chart(selected_day_chart, use_container_width=True)
 
 with tab3: 
-    st.header(f"Occurences over time")
+    st.header(f"Specific word sentiment score over time")
+    selected_tokens = st.multiselect(
+    'Select tokens to visualize:',
+    options=news_long['tokens'].unique(),  # Unique tokens from the dataset
+    default=['biden', 'rate', 'gold'])  # Default selection
+    df_filtered = avg_token_sentiment_over_time[avg_token_sentiment_over_time['tokens'].isin(selected_tokens)]
+    sentiment_over_time_chart = create_plot(df_filtered, x='collection_date', y='sentiment', color='tokens', color_scale='Viridis', title='Sentiment Score over time', plot_type='line')
+    st.plotly_chart(sentiment_over_time_chart, use_container_width=True)
+
+with tab4: 
+    st.header(f"Specific word occurence over time")
+    selected_tokens = st.multiselect(
+    'Select tokens to visualize:',
+    options=news_long['tokens'].unique(),  # Unique tokens from the dataset
+    default=['biden', 'rate', 'gold', 'russia'])  # Default selection
+    df_filtered = token_occurrence_over_time[token_occurrence_over_time['tokens'].isin(selected_tokens)]
+    occurrence_over_time_chart = create_plot(df_filtered, x='collection_date', y='counts', color='tokens', color_scale='Viridis', title='Sentiment Score over time', plot_type='line')
+    st.plotly_chart(occurrence_over_time_chart, use_container_width=True)
